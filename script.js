fe481e2bdfc3b0c70ee2846ff73cdb6c22412d66
wallbrian <116678290+wallbrian@users.noreply.github.com>
2022-11-23 18:21:22 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,17 @@
 function bottle(){
 	for (let i=99; i>=0; i--){
 		let song =""
+		let song2 =""
 		
 			if(i>2){
-				song = +i+ " bottles of beer on the wall, " +i+ " bottles of beer"
-					"Take one down and pass it around, " +(i-1)+ " bottles of beer on the wall"
+				song =  +i+ " bottles of beer on the wall, " +i+ " bottles of beer" 		
+				document.getElementById("main").innerHTML += (song)
+				"+<p>+"
+				song2 = "Take one down and pass it around, " +(i-1)+ " bottles of beer on the wall"
+				document.getElementById("main").innerHTML += (song2)
+				}
 			
-			document.getElementById("main").innerHTML += (song)
-			}
-			
-			else if(i==0){
+			else if(i==1){
 						song= "1 bottle of beer on the wall, 1 bottle of beer"
 							"Take one down and pass it around, no more bottles of beer on the wall"
 							"No more bottles of beer on the wall, no more bottles of beer"
